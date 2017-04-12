6182b12efbf0c1f026f7b469e27c7437092debc6
TheSeventhLion <7catalyst@gmail.com>
2017-04-12 13:37:06 -0600

    project complete

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+var messages= []
 
-const app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
 
 app.use(express.static('assets'))
 app.use(bodyParser.json())
-
-const messages= ["default" , "messages"]
-
+var port = 6001
 
 
-
-app.get('/messages' , function (req,res){
-    res.status(200).json({ anything : messages });
+app.get('/messages' , function (req, res, next){
+    res.status(200).json({ messages : messages });
 
 });
+
+
 
 app.post('/messages', function (req, res, next) {
     console.log(req.body, "<~~this is the REQ.BODY object");
     // messages.push(req.body.message)
     messages.push({ message: req.body.message, time: new Date() }); 
-  res.status(200).json({ anything : messages });
+  res.status(200).json({ messages : messages });
 });
 
 
-
-
-
-
-
-
-
-app.listen(4000, function (){
+app.listen(6001, function (){
     console.log("Go ahead, Im listening!")
 })
 
